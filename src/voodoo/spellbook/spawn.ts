0c2b8f5d6b2172bc4a93dc29e1b1d079068006bc
botboy434 <herzius@outlook.co.nz>
2021-08-25 15:38:01 +1200

    Changing it from a string to a prefab

--- src/voodoo/spellbook/spawn.ts
+++ src/voodoo/spellbook/spawn.ts
@@ -1,8 +1,5 @@
 import { VoodooServer } from '..';
-import { createString, Prefab } from 'att-string-transcoder';
 
-export const spawn = (voodoo: VoodooServer, accountId: number, prefab: Prefab) => {
-  const spawnString = createString(prefab);
-
-  return voodoo.command({ accountId, command: `trade post string-raw ${spawnString}` });
-};
+Export const spawn = (voodoo: VoodooServer, accountId: number, prefabhash: number) => {
+  return voodoo.command({ accountId, command: `trade post ${accountId} ${prefabhash}` });
+}
